32f19f243d78833d461839e4690ad24ec5ce0340
svetlozara-godzheva <n01754024@humber.ca>
2025-08-01 17:50:07 -0400

    Created a student object

---
+++ script.js
@@ -0,0 +1,15 @@
+// Understanding and Creating Objects
+function Student(name, age, enrolled, courses) {
+    this.name = name;
+    this.age = age;
+    this.enrolled = enrolled;
+    this.courses = courses;
+    this.print = function () {
+        console.log(`Student name: ${this.name}`);
+        console.log(`Student age: ${this.age}`);
+        console.log(`Is ${this.name} enrolled: ${this.enrolled}`);
+        console.log(`The courses that ${this.name} enrolls are ${this.courses}`);
+    };
+}
+
+let student1 = new Student("Mathilde", 18, true, ["Chemistry", "Management", "Analysis"]);
