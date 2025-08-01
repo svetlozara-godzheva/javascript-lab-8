8beb789fab4599e447283b518d3c9f0e75d5f188
svetlozara-godzheva <n01754024@humber.ca>
2025-08-01 18:10:52 -0400

    Converted JSON into object

--- script.js
+++ script.js
@@ -22,3 +22,8 @@ student1.print();
 // Working with JSON
 let jsonStudent1 = JSON.stringify(student1);
 console.log(jsonStudent1);
+
+let convertedJson = JSON.parse(jsonStudent1);
+console.log(convertedJson);
+// the converted JSON student and student1 are two different objects in the memory, hence the result would be false:
+console.log(convertedJson === student1);
