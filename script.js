2e7ff0cad0413bd5dda782792c540b8771db9c93
svetlozara-godzheva <n01754024@humber.ca>
2025-08-01 17:54:29 -0400

    Printing the student object info

--- script.js
+++ script.js
@@ -8,8 +8,13 @@ function Student(name, age, enrolled, courses) {
         console.log(`Student name: ${this.name}`);
         console.log(`Student age: ${this.age}`);
         console.log(`Is ${this.name} enrolled: ${this.enrolled}`);
-        console.log(`The courses that ${this.name} enrolls are ${this.courses}`);
+        console.log(`The courses that ${this.name} enrolls in are ${this.courses}`);
     };
 }
 
 let student1 = new Student("Mathilde", 18, true, ["Chemistry", "Management", "Analysis"]);
+
+console.log(student1.name);
+console.log(student1.age);
+
+student1.print();
